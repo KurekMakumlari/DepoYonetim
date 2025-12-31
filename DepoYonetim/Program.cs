c35a8e0ef101eee4d849c030291488d136f9556f
AhmetBulgurcu <ahmetbulgurcuu@gmail.com>
2025-12-31 16:14:08 +0300

    251231/16.14

--- DepoYonetim/Program.cs
+++ DepoYonetim/Program.cs
@@ -16,7 +16,7 @@ namespace DepoYonetim
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PersonelGirisForm());
+            Application.Run(new Forms.Main());
         }
     }
 }
